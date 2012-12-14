da50687a0ac3bd9671fd51623b0e7d6b7adc1687
Andrey Boytsov <aboytsov@gumtree.com>
2012-12-14 10:33:01 +0000

    original commit

---
+++ src/com/gumtree/application/AddressBookApplication.java
@@ -0,0 +1,11 @@
+package com.gumtree.application;
+
+
+public class AddressBookApplication {
+
+    static String FILE = "../AddressBook";
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
