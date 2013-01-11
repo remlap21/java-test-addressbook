c96acd3a34aec02b59f0fa9acbb8ba80aba6fd32
Andrey Boytsov <aboytsov@gumtree.com>
2013-01-11 17:30:45 +0000

    making it a maven project

---
+++ src/main/java/com/gumtree/application/AddressBookApplication.java
@@ -0,0 +1,10 @@
+package com.gumtree.application;
+
+
+public class AddressBookApplication {
+
+    static String FILE = "../AddressBook";
+
+    public static void main(String[] args) throws Exception {
+    }
+}
